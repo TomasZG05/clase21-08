597e8533cf9a21266ba08884451e4b2896581f8f
Sala K 301 <salak301@salasfr.itm.edu.co>
2025-08-21 09:22:26 -0500

    Clase estados

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+
+public class Estados
+{
+    public int id;
+    public String? Nombre;
+    
+}
+
+public class Frutas
+{
+    public int id;
+    public String? Nombre;
+    public decimal Peso;
+    public bool Fresca;
+    public Estados? Estado;
+}
