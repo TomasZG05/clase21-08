56c79fd31a8291f172b2198525bcfe0ba9b51107
Sala K 301 <salak301@salasfr.itm.edu.co>
2025-08-21 09:35:08 -0500

    Datos agregados

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,18 +1,31 @@
 ﻿
 Console.WriteLine("Hello, World!");
 
+Personas persona = new Personas();
+persona.id = 1;
+persona.Nombre = "Pepito Perez";
+persona.Estatura = 1.60m;
+persona.Viva = false;
+persona.Fecha = DateTime.Now;
+persona.Estado = new Estados() { id = 1, Nombre = "Viudo" };
 public class Estados
 {
     public int id;
     public String? Nombre;
-    
+
 }
 
-public class Frutas
+public class Personas
 {
     public int id;
     public String? Nombre;
-    public decimal Peso;
-    public bool Fresca;
+    public decimal Estatura;
+    public bool Viva;
+    public DateTime Fecha;
     public Estados? Estado;
 }
+
+public class Estudiantes : Personas
+{
+    public String? Carnet;
+ }
